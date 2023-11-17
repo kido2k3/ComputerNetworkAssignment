c244d45637cc9db496dd7185395dcf8f38fb7962
Duy Bùi Thanh <btduy12.11lvl@gmail.com>
2023-11-17 08:47:35 +0700

    feat: user not found in dicover

--- FileSharingClient/src/Terminal/ContentDiscoverTerminal/ContentDiscoverTerminal.js
+++ FileSharingClient/src/Terminal/ContentDiscoverTerminal/ContentDiscoverTerminal.js
@@ -4,21 +4,27 @@ import React, { useEffect, useState } from "react";
 export default function ContentDiscoverTerminal({ files, hostname }) {
   return (
     <Box style={{ color: "#fff" }}>
-      <Typography sx={{ textAlign: "center", marginBottom: "20px" }}>
-        THE LIST OF LOCAL FILES OF {hostname}
-      </Typography>
-      {files?.length > 0 ? (
-        <ul>
-          {files?.map((item) => {
-            return (
-              <li style={{ color: "#fff", marginTop: "5px" }}>
-                + {item?.lname}
-              </li>
-            );
-          })}
-        </ul>
+      {files === "user not found" ? (
+        <Typography>User not found</Typography>
       ) : (
-        <Typography>No data to show</Typography>
+        <>
+          <Typography sx={{ textAlign: "center", marginBottom: "20px" }}>
+            THE LIST OF LOCAL FILES OF {hostname}
+          </Typography>
+          {files?.length > 0 ? (
+            <ul>
+              {files?.map((item) => {
+                return (
+                  <li style={{ color: "#fff", marginTop: "5px" }}>
+                    + {item?.lname}
+                  </li>
+                );
+              })}
+            </ul>
+          ) : (
+            <Typography>No data to show</Typography>
+          )}
+        </>
       )}
     </Box>
   );
